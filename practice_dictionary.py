55d537fd087161e0753215c281134571db23cbf3
toastdabunnyiii <toastdabunnyii@gmail.com>
2025-05-16 17:50:19 -0700

    New dictionary practicing

--- practice_dictionary.py
+++ practice_dictionary.py
@@ -10,10 +10,28 @@
 student = {
     "Name": "Bob",
     "Age": "∞",
-    "Grade": "∞"
+    "Grade": "∞" # key value pairs
 }
 
 # print(f"Bob is {student['age']} years old.")
 
 # TODO: Display the name or the grade in the console
 print(f"Once upon a time, there was a dude named {student['Name']}.")
+
+# adding new key-value pair
+# student['fav_color']
+
+# removing a key-value pair
+# def student['fav_color']
+
+# displaying keys of a dictionary
+# print(student.keys())
+
+# coverting values to a list and displaying
+# print(list(student.valuese()))
+
+# print() <-- extra line of spacing
+
+# Loop throught the dictionary and display the key and value pairs one at a time
+for (k, v) in student.items():
+    print(k, v)
