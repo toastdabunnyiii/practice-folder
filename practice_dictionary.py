22c664662243632b7f4bda3aba2a232177b35797
toastdabunnyiii <toastdabunnyii@gmail.com>
2025-05-16 17:21:09 -0700

    updating

---
+++ practice_dictionary.py
@@ -0,0 +1,19 @@
+# d = {
+    # "KeyName0": "Value0",
+    # "KeyName1": 1
+# }
+
+# TODO: Create a dictionary called student
+# Include three keys: name, age, grade
+# Each key needs a value - yourself or random
+
+student = {
+    "Name": "Bob",
+    "Age": "∞",
+    "Grade": "∞"
+}
+
+# print(f"Bob is {student['age']} years old.")
+
+# TODO: Display the name or the grade in the console
+print(f"Once upon a time, there was a dude named {student['Name']}.")
---
+++ practice_lists.py
@@ -0,0 +1,16 @@
+# TODO: Create a list of 5 numbers
+# Print out the first item in the list using the index
+# BONUS: Print out all the items in the list using a for each loop
+
+# TODO: Create a list of three strings
+# Print out the lotal number of items in the list
+# Add one item to end of the list
+# BONUS: Remove one item from the list
+
+num = [67, 6, 9, 14, 53]
+print(num[0])
+
+strings = ['cantalope', 'goji', 'thyme']
+print(len(strings))
+strings.append('cherries')
+# strings.remove('cantalope')
