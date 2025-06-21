4ba2d0bb364128481ba7062973fdd4c6e44665e7
toastdabunnyiii <toastdabunnyii@gmail.com>
2025-06-20 17:17:40 -0700

    fixing bugs

--- Lost_City-adventure/main.py
+++ Lost_City-adventure/main.py
@@ -1,3 +1,19 @@
+# Welcome to the Lost City Adventure!
+'''Location One'''
+# You are seraching for an ancient artifact that was passed down from your family, being guiding by your ancenstors.
+# You come apon a cave, seemingly calling fo ryou. Instead of giving off a creepy vibe, it seems more welcoming.
+'''Question One'''
+# Do you enter the cave?
+    # yes = You enter the cave.
+    # no = You avoid the cave.
+'''Location One (no)'''
+# You explore the forest beyond the cave and soon get tired, so you make a camp and go to sleep. The next day, you wake up to see a spirit wandering around.
+# When it notices you watching them, they approach toward you.
+# 'Hello,' they say, and you can make out a warm smile. Your grandma. 'I have come here to give you a quest. I am aware you are already 
+
+'''Location Two (yes)'''
+# You wander into the cave and can make out a silhouette, seemingly not dangerous.
+
 import time
 
 def game():
@@ -9,7 +25,7 @@ def game():
 
     time.sleep(3)
 
-    print('You are searching for an ancient artifact that was passed down from your family. You are being guided by your ancestors.')
+    print('You are searching for an ancient artifact that was passed down from your family, being guided by your ancestors.')
     print('You come apon a cave, seemingly calling for you. Instead of giving off a creepy vibe, it seems more welcoming.')
 
     choice = print_choice('Do you enter the cave?')
@@ -38,3 +54,5 @@ def print_choice(choice):
     if user_input in ['Y', 'y', 'yes', 'YES', 'yES', 'yeS', 'Yes', 'yEs', 'YeS', 'YEs', 'ye', 'YE', 'yE', 'Ye']:
         return True
     return False
+
+game()
