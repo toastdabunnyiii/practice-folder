d2259c485a1871e665fabc81f622c7117c7a3b74
toastdabunnyiii <toastdabunnyii@gmail.com>
2025-06-20 18:04:22 -0700

    5/20/25 : : Creating the first response.

--- Lost_City-adventure/main.py
+++ Lost_City-adventure/main.py
@@ -1,19 +1,3 @@
-# Welcome to the Lost City Adventure!
-'''Location One'''
-# You are seraching for an ancient artifact that was passed down from your family, being guiding by your ancenstors.
-# You come apon a cave, seemingly calling fo ryou. Instead of giving off a creepy vibe, it seems more welcoming.
-'''Question One'''
-# Do you enter the cave?
-    # yes = You enter the cave.
-    # no = You avoid the cave.
-'''Location One (no)'''
-# You explore the forest beyond the cave and soon get tired, so you make a camp and go to sleep. The next day, you wake up to see a spirit wandering around.
-# When it notices you watching them, they approach toward you.
-# 'Hello,' they say, and you can make out a warm smile. Your grandma. 'I have come here to give you a quest. I am aware you are already 
-
-'''Location Two (yes)'''
-# You wander into the cave and can make out a silhouette, seemingly not dangerous.
-
 import time
 
 def game():
@@ -30,11 +14,23 @@ def game():
 
     choice = print_choice('Do you enter the cave?')
     print(choice)
+    
+    if choice == True:
+        print('You wander into the cave and can make out a silhouette, seemingly not dangerous.')
+
+    elif choice == False:
+        print('You explore the forest beyond the cave and soon get tired, so you make a camp and go to sleep.')
+        print('')
+        print('-THE NEXT DAY-')
+        print('')
+        print('The next day, you wake up to see a spirit wandering around. When it notices you watching them, they approach toward you.')
+        print(f'"Hello, {name}," they say, and you can make out a warm smile. Your grandma. "I have come here to give you a quest. I am aware you are already on one, but this should help you.')
+        print(f'"There is a lost city you need to find," she continues. "To complete this quest, you must find a.. sidekick, as you call them these days."')
 
 inventory = {
     'torch': 0,
     'compass (uses)': 3,
-    'sword': 1,
+    'sword': 1
 }
 
 def print_inventory():
@@ -48,7 +44,7 @@ def print_inventory():
 def print_choice(choice):
     user_input = 'i'
     while (user_input == 'i'):
-        user_input = input(f"{choice} [yes/no] \n Enter 'i' to see your inventory") # '\n' means 'new line'
+        user_input = input(f"{choice} [yes/no] \nEnter 'i' to see your inventory ") # '\n' means 'new line'
         if (user_input == 'i'):
             print_inventory()
     if user_input in ['Y', 'y', 'yes', 'YES', 'yES', 'yeS', 'Yes', 'yEs', 'YeS', 'YEs', 'ye', 'YE', 'yE', 'Ye']:
@@ -56,3 +52,23 @@ def print_choice(choice):
     return False
 
 game()
+
+
+# --------------------------------------------------------------------------------#
+
+# Welcome to the Lost City Adventure!
+'''Location One'''
+# You are seraching for an ancient artifact that was passed down from your family, being guiding by your ancenstors.
+# You come apon a cave, seemingly calling fo ryou. Instead of giving off a creepy vibe, it seems more welcoming.
+'''Question One'''
+# Do you enter the cave?
+    # yes = You enter the cave.
+    # no = You avoid the cave.
+'''Location One (no)'''
+# You explore the forest beyond the cave and soon get tired, so you make a camp and go to sleep. The next day, you wake up to see a spirit wandering around.
+# When it notices you watching them, they approach toward you.
+# 'Hello,' they say, and you can make out a warm smile. Your grandma. 'I have come here to give you a quest. I am aware you are already on one,
+# but this should help you.
+
+'''Location Two (yes)'''
+# You wander into the cave and can make out a silhouette, seemingly not dangerous.
